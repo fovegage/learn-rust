47deb85fa2af5e12f37240ff95946c92c5883faf
fovegage <fovegage@gmail.com>
2022-10-13 14:13:33 +0800

    test

--- demo/dll_test/test_dll.py
+++ demo/dll_test/test_dll.py
@@ -1,6 +1,12 @@
-from ctypes import CDLL, WinDLL
+def win_test():
+    from ctypes import WinDLL
+    # mac
+    lib = WinDLL("/Users/gaozhe/GolandProjects/learn-rust/demo/dll-demo/target/release/dll.dll")
+    #
+    lib.hello()
 
-# mac
-lib = WinDLL("/Users/gaozhe/GolandProjects/learn-rust/demo/dll-demo/target/release/dll.dll")
-#
-lib.hello()
+
+def mac_test():
+    from ctypes import CDLL
+    lib = CDLL(r"../dll-demo/target/release/dll.dll")
+    lib.hello()
